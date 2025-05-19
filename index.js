97f1454cdba47579e475fe0bbc7dede4ac5e6069
RomanDiomin <158568967+RomanDiomin@users.noreply.github.com>
2025-05-20 01:08:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,19 @@ const host = 'localhost';
 const port = 8000;
 
 const server = http.createServer((req, res) => {
-    // Replace this code by your own
-    res.writeHead(200);
-    res.end('Hello from Server!!!!');
+    // перевірка наявності Authorization header і його значення
+  if (req.headers.authorization === 
+    "BearerekV5Rk4wMlgvYVpCbmp5WUh5bHVPMktwMzktY05QeDRjT3FlWlNiUTJhbVpraHc5d3Y5a3YtU2pM") {
+    // якщо Authorization header містить відповідне значення, повертаємо статус 200
+    res.writeHead(200);
+    res.end('OK');
+  } else {
+    // якщо Authorization header відсутній або містить неправильне значення, повертаємо статус 401
+    res.writeHead(401);
+    res.end('Unauthorized');
+  }
 });
 
 server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+    console.log(`Server is running on http://${host}:${port}`);
 });
